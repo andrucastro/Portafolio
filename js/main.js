f6b9436f240f7fe9b838c7d3b6a9b580fa0cc044
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2020-09-19 18:49:40 -0500

    final

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 // Links assignments 
+const links = [ 
+    {
+    label: "week1",
+    link:"/week1/reading_assignment.html"
+    },
 
-links_assignmets = ["week1/readin_assignmet.html","week1/readin_notes.html"]; 
+    {
+    label: "week2",
+    link: "/week1/reading_assignment.html"    
+    }
+];
+
+//number of links on the list
+var links_number = links.length;
+
+console.log(links[1].label)
+
 
 function add_link(){
 
-    var ol_elemnt = document.getElementById("content_table");
-    var new_link = document.createElement('li');
-    
-    console.log(new_link);
-} 
+    //create a link per every item in the array 
+  i = 0;    
+  while (i <= links_number){
+        
+        const content_table = document.getElementById("content_table");
+
+        var newElement = document.createElement('a');
+
+        newElement.innerHTML =links[i].label;
+        newElement.href = links[i].links;
+        
+        i++;
+    }
+}
+
+add_link();
