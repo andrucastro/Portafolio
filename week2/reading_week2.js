d9a7a9266c70718900d8bb3a10c40c52f5e364dc
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2020-09-23 09:43:14 -0500

    week to start

---
+++ week2/reading_week2.js
@@ -0,0 +1,9 @@
+// store the question in the variable 
+const question = "What is Superman's real name?"
+
+//display a pormt to the user wiht the question 
+const answer = prompt(question);
+
+
+alert(`You answered ${answer}`);
+
