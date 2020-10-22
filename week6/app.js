4a0bf7aecaadc957af562a7d51d852a44314b006
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2020-10-21 22:12:29 -0500

    almost ready

--- week6/app.js
+++ week6/app.js
@@ -1,7 +1,7 @@
 // Selectors 
 const input = document.querySelector(".input_task_field");
 const todobutton = document.querySelector(".todo_button");
-const todoList = document.querySelector(".todo_button");
+const todoList = document.querySelector(".todo_list");
 
 //Event listeners 
 
@@ -11,15 +11,34 @@ todobutton.addEventListener("click", addTodo);
 
 function addTodo(event){
 
+    // prevent submint the form 
     event.preventDefault();
 
+    //create a new div container for every task 
     const divTaks = document.createElement('div');
     divTaks.classList.add("todo")
 
+    // create a task 
     const newTaks = document.createElement('li');
+
     newTaks.innerText = "test";
     newTaks.classList.add('task-item')
 
+    // insert the new task inside a div container
     divTaks.appendChild(newTaks);
 
+    // create a new elemnt button and inser an icon
+    const completedBtn = document.createElement('button');
+    completedBtn.innerHTML='<i class="far fa-check-circle"></i>';
+    completedBtn.classList.add("complete-btn");
+    divTaks.appendChild(completedBtn);
+
+    // create a new elemnt button and inser an icon
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML='<i class="fas fa-trash-alt"></i>';
+    deleteBtn.classList.add("complete-btn");
+    divTaks.appendChild(deleteBtn);
+
+    //insert the new Div Item within the Ul tag with the .todo list"
+    todoList.appendChild(divTaks)
 }
