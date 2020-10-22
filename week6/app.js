27cbc3875b044a95f5e69d0df5f18c50ea112b15
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2020-10-21 23:18:42 -0500

    veamos a ver

--- week6/app.js
+++ week6/app.js
@@ -21,22 +21,24 @@ function addTodo(event){
     // create a task 
     const newTaks = document.createElement('li');
 
-    newTaks.innerText = "test";
-    newTaks.classList.add('task-item')
-
-    // insert the new task inside a div container
-    divTaks.appendChild(newTaks);
-
     // create a new elemnt button and inser an icon
     const completedBtn = document.createElement('button');
     completedBtn.innerHTML='<i class="far fa-check-circle"></i>';
     completedBtn.classList.add("complete-btn");
     divTaks.appendChild(completedBtn);
 
+    //insert the text of input in the item
+    newTaks.innerText = input.value;
+    newTaks.classList.add('task-item')
+
+    // insert the new task inside a div container
+    divTaks.appendChild(newTaks);
+
+
     // create a new elemnt button and inser an icon
     const deleteBtn = document.createElement('button');
-    deleteBtn.innerHTML='<i class="fas fa-trash-alt"></i>';
-    deleteBtn.classList.add("complete-btn");
+    deleteBtn.innerHTML='<i class="fas fa-trash"></i>';
+    deleteBtn.classList.add("delete-btn");
     divTaks.appendChild(deleteBtn);
 
     //insert the new Div Item within the Ul tag with the .todo list"
