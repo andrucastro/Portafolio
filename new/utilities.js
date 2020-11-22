b822767b4c6f3d2d219e01bb084062fa959189b6
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2020-11-21 20:32:39 -0500

    forms

---
+++ new/utilities.js
@@ -0,0 +1,21 @@
+
+const link ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02' 
+
+function getJSON(url){
+
+return fetch(url)
+    .then(function(response){
+        if (!response.ok) {
+            throw Error(response.statusText);
+        } else {
+            return response.json();
+        }
+    })
+
+    .catch(function(error) {
+        console.log(error);
+    });
+
+}
+
+getJSON(link)
