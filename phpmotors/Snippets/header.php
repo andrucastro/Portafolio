f9173f7565d51aeba0b6987978c80016b6f7e989
Andres Castro <guevaracastroandres@gmail.com>
2021-05-01 18:03:04 -0500

    mas

---
+++ phpmotors/Snippets/header.php
@@ -0,0 +1,15 @@
+
+    <div class="logo_container_top">
+        <img src="/phpmotors/images/site/logo.png" alt="PHP motors logo">
+        <span><a href="">My account</a></span>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="/phpmotors/index.php">Home</a></li>
+            <li><a href="/phpmotors/classic.php">Classic</a></li>
+            <li><a href="/phpmotors/sports.php">Sports</a></li>
+            <li><a href="/phpmotors/suv.php">SUV</a></li>
+            <li><a href="/phpmotors/truckcs.php">Truckcs</a></li>
+            <li><a href="/phpmotors/used.php">Used</a></li>
+        </ul>
+    </nav>
